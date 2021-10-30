942bc24fec589b83ca6accff13c485b5d1716f95
Mehdi Mehrabani <mmehr1988@gmail.com>
2021-10-29 23:44:14 -0400

    added functionality for redirecting user to the create exercise page if user clicks on the add button while viewing historical exercises

--- public/assets/js/workouts.js
+++ public/assets/js/workouts.js
@@ -1,7 +1,9 @@
 const view_btn = document.querySelectorAll('.hist-view-button');
 const delete_btn = document.querySelectorAll('.hist-delete-button');
 const delete_exercise_btn = document.querySelectorAll('.delete-exercise');
+const add_btn = document.querySelectorAll('.hist-add-button');
 
+// [1] TO VIEW ONE WORKOUT
 if (view_btn) {
   view_btn.forEach((element) =>
     element.addEventListener('click', async function (event) {
@@ -11,6 +13,7 @@ if (view_btn) {
   );
 }
 
+// [2] TO DELETE ONE WORKOUT
 if (delete_btn) {
   delete_btn.forEach((element) =>
     element.addEventListener('click', async function (event) {
@@ -20,12 +23,23 @@ if (delete_btn) {
   );
 }
 
+// [3] TO DELETE ONE EXERCISE ONCE USER ENTERS WORKOUT
 if (delete_exercise_btn) {
   delete_exercise_btn.forEach((element) =>
     element.addEventListener('click', async function (event) {
       const exId = event.target.dataset.id;
       const wktId = window.location.pathname.split('/').pop();
       await API.deleteOneExercise(exId, wktId);
+    })
+  );
+}
+
+// [4] TO REDIRECT USER TO THE ADD EXERCISE PAGE
+if (add_btn) {
+  add_btn.forEach((element) =>
+    element.addEventListener('click', async function (event) {
+      const id = event.target.dataset.id;
+      await API.goToCreatePage(id);
     })
   );
 }
